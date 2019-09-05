508b88385351f32c51db07fea501e2edf99cfae1
Bella Manoim <bella458@yahoo.com>
2019-09-04 21:30:59 -0400

    updated triangle files

--- TestTriangle.py
+++ TestTriangle.py
@@ -5,6 +5,7 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 
 @author: jrr
 @author: rk
+@author: bella manoim
 """
 
 import unittest
@@ -19,13 +20,28 @@ class TestTriangles(unittest.TestCase):
 
     def testRightTriangleA(self): 
         self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
-
+        
     def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+    
+    def testRightTriangleC(self): 
+        self.assertEqual(classifyTriangle(5,12,13),'Right','5,12,13 is a Right triangle')
+    
+    def testIsocelesTriangle(self): 
+        self.assertEqual(classifyTriangle(5,5,4),'Isoceles','5,5,4 is a Isoceles triangle')
         
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+    
+    def testInvalidInputA(self): 
+        self.assertEqual(classifyTriangle(0,0,1),'InvalidInput','0,0,1 is invalid')
 
+    def testInvalidInputB(self): 
+        self.assertEqual(classifyTriangle(0,0,0),'InvalidInput','0,0,0 is invalid')
+
+    def testInvalidInputC(self): 
+        self.assertEqual(classifyTriangle('a','b',0),'InvalidInput','a,b,0 is invalid')
+        
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
--- Triangle.py
+++ Triangle.py
@@ -7,6 +7,7 @@ The primary goal of this file is to demonstrate a simple python program to class
 
 @author: jrr
 @author: rk
+@author: Bella Manoim
 """
 
 def classifyTriangle(a,b,c):
@@ -30,28 +31,37 @@ def classifyTriangle(a,b,c):
     # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
-        
-    if a <= 0 or b <= b or c <= 0:
+    
+    # @bm fixed that b should be <=0 not b<=b
+    if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
     
     # verify that all 3 inputs are integers  
     # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
+    if not(isinstance(a,int) or isinstance(b,int) or isinstance(c,int)):
         return 'InvalidInput';
       
     # This information was not in the requirements spec but 
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    # @bm fixed the logic here to add instead of subtract the two sides that the third should be greater than
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
-    if a == b and b == a:
+    # @bm fixed that we need to check a = b = c for an equilateral
+    if a == b and b == c:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+    # @bm we were multiplying by 2 instead of squaring. fixed to square the sides and check for all sides
+    elif ((a * a) + (b * b)) == (c * c):
+        return 'Right'
+    elif ((a * a) + (c * c)) == (b * b):
+        return 'Right'
+    elif ((c * c) + (b * b)) == (a * a):
         return 'Right'
     elif (a != b) and  (b != c) and (a != b):
         return 'Scalene'
-    else:
+    # @bm added logic for isoceles
+    elif ((a == b and a != c) or (b == c and b != a) or (a == c and a != b)):
         return 'Isoceles'
